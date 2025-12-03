71c50f6ebf0007688d8b9185b07b51a2c62778b0
MartaBastiani <martabastiani@edu.unifi.it>
2025-12-03 16:05:27 +0100

    fix: annotation

--- R/count_proteins.R
+++ R/count_proteins.R
@@ -14,10 +14,17 @@ opt <- parse_args(OptionParser(option_list=option_list))
 analysis_id <- c(opt$id)
 
 proteinData <- read.delim(paste0("data/proteins/", analysis_id, "/proteinGroups.txt"), sep="\t")
+data <- proteinData[proteinData$Gene.names != "", ] # removing proteins without a gene symbol
+data <- data[!grepl(";", data$Gene.names), ] # important: removing proteinGroups with multiple genes identified.
 
-sel <- grep(pattern = "corrected", names(proteinData))
-data <- proteinData[, sel]
-data_sel <- data[apply(data, 1, sum) > 0, ]
+sel <- grep(pattern = "corrected", names(proteinData)) 
+data <- aggregate(data[, sel], by = list(Gene.names = data$Gene.names), FUN = sum) # sum of identical genes
+rownames(data) <- data$Gene.names
+data$Gene.names <- NULL 
+
+countdata <- data[apply(data, 1, sum) > 1, ]
+
+colnames(countdata) <- gsub("Reporter.intensity.corrected.", "", colnames(countdata))
 
 file_out <- paste0("data/proteins/", analysis_id, "/to_DEA.tsv")
-write.table(data_sel, file_out, sep="\t", quote=F, row.names=F)
+write.table(countdata, file_out, sep="\t", quote=F, row.names=T)
