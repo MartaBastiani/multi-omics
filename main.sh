9810ca3fbccdecb564b8f92c2bf96d6f81105568
MartaBastiani <martabastiani@edu.unifi.it>
2026-01-31 17:56:56 +0100

    feat: methylomics

--- main.sh
+++ main.sh
@@ -4,6 +4,14 @@ Rscript R/DEA_limma.R -i prova -o proteins 2> DEA_limma_pro.log
 Rscript R/volcano_plot.R -i prova -o proteins 2> volcano_plot_pro.log
 
 
+# methylations
+# genome preparation
+# bismark_genome_preparation --verbose dbs/genomes/
+Rscript bash/meth_map.sh -i prova 2> meth_map.log
+Rscript R/meth_import_alignment.R -i prova 2> meth_import.log
+Rscript R/meth_DEA.R -i prova 2> meth_DEA.log
+
+
 # miRNA
 Rscript R/count_miRNA.R -i prova 2> count_miRNA.log
 Rscript R/DEA_limma.R -i prova -o miRNA 2> DEA_limma_miRNA.log
